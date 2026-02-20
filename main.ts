c57cd9d3aaf28d8917f62dde04fe08187f3e5e81
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2026-02-21 00:14:27 +0100

    Oppdater main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ namespace høydeberegning {
      * Beregner barometrisk høyde basert på trykk og temperatur.
      */
     //% block="barometrisk høyde (m) for|p = $p (Pa)|p1 = $p1 (Pa)|T1 = $T1 (K)|a = $a (K/m)|h1 = $h1 (m)|R = $R (J/(kg·K))|g0 = $g0 (m/s²)"
-    //% blockSetVariable="h"
     //% group="Barometrisk formel"
     //% weight=100
     //% p.defl=101325 p1.defl=101325 T1.defl=288 a.defl=0.0065 h1.defl=0 R.defl=287 g0.defl=9.81
